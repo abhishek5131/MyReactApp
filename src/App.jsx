12bf3c755072bfac1ab88a44241defbbb6100eb8
Abhishek5131 <abhishek5131singh@gmail.com>
2023-07-03 09:17:10 +0530

    using Stateful lists by {useState} and array.map funct to update list dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import ExpenseItem from './components/ExpenseItem'
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense' 
 
+
+const DummyExpenses = [
+  { id: 'e1', title: 'Tissue Paper', amount: 'Rs '+145, date: new Date(2023, 2, 12) },
+  { id: 'e2', title: 'Bike Insurance', amount: 'Rs '+2345, date: new Date(2023, 1, 18) },
+  { id: 'e3', title: 'Electricity Bill', amount: 'Rs '+1518, date: new Date(2022, 11, 27) },
+  { id: 'e4', title: 'Rashan saman', amount: 'Rs '+7278, date: new Date(2022, 10, 16) }
+]
+
 const App = () => {
 
-  const expenses = [
-    { id: 'e1', title: 'Tissue Paper', amount: 'Rs '+145, date: new Date(2023, 2, 12) },
-    { id: 'e2', title: 'Bike Insurance', amount: 'Rs '+2345, date: new Date(2023, 1, 18) },
-    { id: 'e3', title: 'Electricity Bill', amount: 'Rs '+1518, date: new Date(2022, 11, 27) },
-    { id: 'e4', title: 'Rashan saman', amount: 'Rs '+7278, date: new Date(2022, 10, 16) }
-  ]
-
-  const addExpenseHandler = expense => {
-    console.log('in App.jsx');
-    console.log(expense);
+  const [expenses, setExpenses] = useState(DummyExpenses)
+  
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    })
   }
 
   return (
